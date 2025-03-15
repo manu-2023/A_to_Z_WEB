afa0227a1ebc3621f36e7d269cfd416f2536f606
Manu M <128204246+manu-2023@users.noreply.github.com>
2025-03-15 10:58:14 +0530

    Update MailConfig.java

--- a-to-zbackend/src/main/java/com/example/demo/MailConfig/MailConfig.java
+++ a-to-zbackend/src/main/java/com/example/demo/MailConfig/MailConfig.java
@@ -16,8 +16,8 @@ public class MailConfig {
         JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
         mailSender.setHost("smtp.gmail.com");
         mailSender.setPort(587);
-        mailSender.setUsername("noreplyhostel2@gmail.com");
-        mailSender.setPassword("mluetrfdfzhorxzb");
+        mailSender.setUsername("Your Email");
+        mailSender.setPassword("Your Password");
 
         Properties props = mailSender.getJavaMailProperties();
         props.put("mail.transport.protocol", "smtp");
